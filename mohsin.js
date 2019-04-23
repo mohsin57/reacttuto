f3e39dcb9761b997c9eea548ee90992beda8f644
mohsin57 <mohsinali_syed@hotmail.com>
2019-04-23 13:02:36 +0500

    2nd

---
+++ mohsin.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class mohsin extends Component {
+    render() {
+      return (
+        <div>
+            <form>
+                <input type="email">
+                </input>
+                <input type="password"></input>
+            </form>
+        </div>
+      );
+    }
+  }
+  
+export default mohsin;
---
+++ src/New.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class New extends Component {
+    render() {
+      return (
+        <h1> My Name is Mohsin Hello abc</h1>
+        
+      );
+    }
+  }
+  
+export default New;
